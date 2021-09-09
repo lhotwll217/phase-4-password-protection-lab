53783853cb6263deaeab3bb3f861f533cc53a250
lhotwll217 <lukehenryotwell@gmail.com>
2021-09-08 20:07:29 -0400

    commit tree 71d1efc56210d639b94bf2e497f1f90639608f84

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+    wrap_parameters format: []
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user, status: :created 
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+
+    private 
+    
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
